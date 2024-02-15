8ee44d4aa5cd476b470a7c333b0f91cc2ac17858
Angela Cortecchia <angela.cortecchia@studio.unibo.it>
2024-02-15 23:43:42 +0100

    refactor: change structure

--- collektive-benchmark/src/main/kotlin/it/unibo/collektive/examples/time/stateChange.kt
+++
@@ -1,6 +0,0 @@
-package it.unibo.collektive.examples.time
-
-import it.unibo.collektive.aggregate.api.Aggregate
-import it.unibo.collektive.aggregate.api.operators.neighboringViaExchange
-
-fun Aggregate<Int>.stateChange(id: Int): Int = neighboringViaExchange(id * 2).localValue
