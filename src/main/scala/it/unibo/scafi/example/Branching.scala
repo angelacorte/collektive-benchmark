e0cdf5921c65dcd3ed8db132a5f0f6dd97b01f00
Angela Cortecchia <angela.cortecchia@studio.unibo.it>
2024-02-20 16:29:48 +0100

    fix: add sensor molecule

--- src/main/scala/it/unibo/scafi/example/Branching.scala
+++ src/main/scala/it/unibo/scafi/example/Branching.scala
@@ -1,7 +1,7 @@
 package it.unibo.scafi.example
 import it.unibo.alchemist.model.scafi.ScafiIncarnationForAlchemist._
 
-class Branching extends AggregateProgram with StandardSensors {
+class Branching extends AggregateProgram {
   override def main(): Any = branch(sense[Boolean]("sensor")) {
       foldhood(0)((a, b) => a + b)(nbr(1))
     } {
