e78e456ad902abdf1778f457fc42f219882e1021
Angela Cortecchia <angela.cortecchia@studio.unibo.it>
2024-02-20 15:42:56 +0100

    feat: add branching in different incarnations

---
+++ src/main/kotlin/it/unibo/collektive/examples/branching/Branching.kt
@@ -0,0 +1,12 @@
+package it.unibo.collektive.examples.branching
+
+import it.unibo.collektive.aggregate.api.Aggregate
+import it.unibo.collektive.aggregate.api.operators.neighboringViaExchange
+import it.unibo.collektive.field.Field.Companion.hood
+
+fun Aggregate<Int>.branching(cond: Boolean) =
+    if (cond) {
+        neighboringViaExchange(1).hood(0) { acc, _ -> acc + 1 }
+    } else {
+        0
+    }
--- src/main/kotlin/it/unibo/collektive/examples/entrypoint/SimulationEntrypoint.kt
+++ src/main/kotlin/it/unibo/collektive/examples/entrypoint/SimulationEntrypoint.kt
@@ -2,6 +2,8 @@ package it.unibo.collektive.examples.entrypoint
 
 import it.unibo.collektive.aggregate.api.Aggregate
 import it.unibo.collektive.alchemist.device.DistanceSensor
+import it.unibo.collektive.examples.branching.branching
+import it.unibo.collektive.examples.channel.channelWithObstacles
 import it.unibo.collektive.examples.gradient.gradient
 import it.unibo.collektive.examples.neighbors.neighborCounter
 import it.unibo.collektive.examples.time.fieldEvolution
@@ -21,3 +23,8 @@ fun Aggregate<Int>.neighborCounterEntrypoint(): Int = neighborCounter()
  * The entrypoint of the simulation running a state change.
  */
 fun Aggregate<Int>.fieldEvolutionEntrypoint(): Int = fieldEvolution()
+
+context(DistanceSensor)
+fun Aggregate<Int>.channelEntrypoint() = channelWithObstacles(true, localId == 0, true)
+
+fun Aggregate<Int>.branchingEntrypoint(): Int = branching(localId == 0)
