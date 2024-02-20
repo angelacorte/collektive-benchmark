3c55cd97b411e1bc8f65fc445e2ea5b84a16d588
Angela Cortecchia <angela.cortecchia@studio.unibo.it>
2024-02-20 15:40:32 +0100

    feat: update field evolution

--- src/main/kotlin/it/unibo/collektive/examples/time/fieldEvolution.kt
+++ src/main/kotlin/it/unibo/collektive/examples/time/fieldEvolution.kt
@@ -1,6 +1,5 @@
 package it.unibo.collektive.examples.time
 
 import it.unibo.collektive.aggregate.api.Aggregate
-import it.unibo.collektive.aggregate.api.operators.neighboringViaExchange
 
-fun Aggregate<Int>.fieldEvolution(): Int = repeat(0){ it + 1}
+fun Aggregate<Int>.fieldEvolution(): Int = repeat(0) { it + 1 }
