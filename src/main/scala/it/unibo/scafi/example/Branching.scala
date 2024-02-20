58a81c7a9c382a4c3bf0a75774fde3eb430ff0de
Angela Cortecchia <angela.cortecchia@studio.unibo.it>
2024-02-20 10:27:31 +0100

    feat: add scafi examples

---
+++ src/main/scala/it/unibo/scafi/example/Branching.scala
@@ -0,0 +1,10 @@
+package it.unibo.scafi.example
+import it.unibo.alchemist.model.scafi.ScafiIncarnationForAlchemist._
+
+class Branching extends AggregateProgram with StandardSensors {
+  override def main(): Any = branch(sense[Boolean]("sensor")) {
+      foldhood(0)((a, b) => a + b)(nbr(1))
+    } {
+      0
+    }
+}
