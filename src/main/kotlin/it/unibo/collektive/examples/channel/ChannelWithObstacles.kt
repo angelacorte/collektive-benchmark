6e064797f9ace7484b3c7694d1a24b293c10bc93
Angela Cortecchia <angela.cortecchia@studio.unibo.it>
2024-02-20 22:40:58 +0100

    feat: add collektive channel with obstacles

--- src/main/kotlin/it/unibo/collektive/examples/channel/ChannelWithObstacles.kt
+++ src/main/kotlin/it/unibo/collektive/examples/channel/ChannelWithObstacles.kt
@@ -2,22 +2,46 @@ package it.unibo.collektive.examples.channel
 
 import it.unibo.alchemist.device.LocalSensing
 import it.unibo.collektive.aggregate.api.Aggregate
+import it.unibo.collektive.aggregate.api.operators.share
 import it.unibo.collektive.alchemist.device.DistanceSensor
-
-context(DistanceSensor)
-fun Aggregate<Int>.channelWithObstacles(obstacle: Boolean, source: Boolean, target: Boolean) {
-    if (obstacle) {
-        false
-    } else {
-        distances().map { it }
-    }
-
-//    fun channel(obstacle: Boolean, source: Boolean, target: Boolean) = TODO()
-
-    //distances().map { it == }
-}
-
-fun Aggregate<Int>.channel() = TODO()
+import it.unibo.collektive.examples.gradient.gradient
+import it.unibo.collektive.field.Field.Companion.hood
+import it.unibo.collektive.field.plus
+import kotlin.Double.Companion.POSITIVE_INFINITY
+import kotlin.math.min
 
 context(LocalSensing, DistanceSensor)
-fun Aggregate<Int>.channelEntrypoint() = channelWithObstacles(sense("obstacle"), localId == 0, sense("target"))
+fun Aggregate<Int>.channelWithObstacles(): Any =
+    if (sense("obstacle")) {
+        false
+    } else {
+        channel(sense("source"), sense("target"), channelWidth = 1.0)
+    }
+
+context(DistanceSensor)
+fun Aggregate<Int>.channel(source: Boolean, target: Boolean, channelWidth: Double): Boolean =
+    gradient(source) + gradient(target) <= distanceBetween(source, target) + channelWidth
+
+context(DistanceSensor)
+fun Aggregate<Int>.distanceBetween(source: Boolean, target: Boolean): Double = broadcast(source, gradient(target))
+
+context(DistanceSensor)
+fun <A>Aggregate<Int>.broadcast(source: Boolean, value: A): A = gradientCast(source, value) { it }
+
+context(DistanceSensor)
+fun <A>Aggregate<Int>.gradientCast(source: Boolean, initial: A, accumulate: (A) -> A): A =
+    share(POSITIVE_INFINITY to initial) {
+        val gradient = distances() + it.map { it.first }
+        val accumulated = it.map { accumulate(it.second) }
+        val combined = gradient.alignedMap(accumulated) { g, a -> g to a }
+        val minTuple = combined.toMap().minBy { it.value.first }
+        if (source) {
+            0.0 to initial
+        } else {
+            if (minTuple.value.first == POSITIVE_INFINITY) {
+                POSITIVE_INFINITY to initial
+            } else {
+                minTuple.value
+            }
+        }
+    }.second
