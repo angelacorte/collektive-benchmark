9add2652caa2e82b1504d95b31cfeae6592b0de0
Angela Cortecchia <angela.cortecchia@studio.unibo.it>
2024-02-28 15:20:43 +0100

    fix: add control infinite on channel value

--- src/main/kotlin/it/unibo/collektive/examples/channel/ChannelWithObstacles.kt
+++ src/main/kotlin/it/unibo/collektive/examples/channel/ChannelWithObstacles.kt
@@ -1,15 +1,16 @@
 package it.unibo.collektive.examples.channel
 
-import it.unibo.alchemist.device.LocalSensing
 import it.unibo.collektive.aggregate.api.Aggregate
 import it.unibo.collektive.aggregate.api.operators.share
-import it.unibo.collektive.alchemist.device.DistanceSensor
+import it.unibo.collektive.alchemist.device.sensors.DistanceSensor
+import it.unibo.collektive.alchemist.device.sensors.LocalSensing
 import it.unibo.collektive.examples.gradient.gradient
-import it.unibo.collektive.field.Field.Companion.hood
-import it.unibo.collektive.field.plus
+import it.unibo.collektive.field.Field.Companion.fold
 import kotlin.Double.Companion.POSITIVE_INFINITY
-import kotlin.math.min
 
+/**
+ * Compute the channel between the source and the target with obstacles.
+ */
 context(LocalSensing, DistanceSensor)
 fun Aggregate<Int>.channelWithObstacles(): Any =
     if (sense("obstacle")) {
@@ -18,30 +19,47 @@ fun Aggregate<Int>.channelWithObstacles(): Any =
         channel(sense("source"), sense("target"), channelWidth = 0.3)
     }
 
+/**
+ * Compute the channel between the [source] and the [target] with a specific [channelWidth].
+ */
 context(DistanceSensor)
-fun Aggregate<Int>.channel(source: Boolean, target: Boolean, channelWidth: Double): Boolean =
-    gradient(source) + gradient(target) <= distanceBetween(source, target) + channelWidth
+fun Aggregate<Int>.channel(source: Boolean, destination: Boolean, channelWidth: Double): Boolean {
+    val distancesToSource = gradient(source)
+    val distanceToDestination = gradient(destination)
+    val dBetween = distanceBetween(source, destination)
+    return !((distancesToSource + distanceToDestination).isInfinite() && dBetween.isInfinite()) &&
+        distancesToSource + distanceToDestination <= dBetween + channelWidth
+}
 
+/**
+ * Compute the distance between the [source] and the [target].
+ */
 context(DistanceSensor)
 fun Aggregate<Int>.distanceBetween(source: Boolean, target: Boolean): Double = broadcast(source, gradient(target))
 
+/**
+ * Computes the [gradientCast] from the [source] with the [value] that is the distance from the [source] to the target.
+ */
 context(DistanceSensor)
-fun <A>Aggregate<Int>.broadcast(source: Boolean, value: A): A = gradientCast(source, value) { it }
+fun Aggregate<Int>.broadcast(source: Boolean, value: Double): Double = gradientCast(source, value) { it }
 
+/**
+ * Compute the gradient of the aggregate from the [source] to the [target].
+ * The [accumulate] function is used to accumulate the value of the aggregate.
+ */
 context(DistanceSensor)
-fun <A>Aggregate<Int>.gradientCast(source: Boolean, initial: A, accumulate: (A) -> A): A =
-    share(POSITIVE_INFINITY to initial) {
-        val gradient = distances() + it.map { it.first }
-        val accumulated = it.map { accumulate(it.second) }
-        val combined = gradient.alignedMap(accumulated) { g, a -> g to a }
-        val minTuple = combined.toMap().minBy { it.value.first }
-        if (source) {
-            0.0 to initial
-        } else {
-            if (minTuple.value.first == POSITIVE_INFINITY) {
-                POSITIVE_INFINITY to initial
-            } else {
-                minTuple.value
+fun Aggregate<Int>.gradientCast(source: Boolean, initial: Double, accumulate: (Double) -> Double): Double =
+    share(POSITIVE_INFINITY to initial) { field ->
+        val dist = distances()
+        when {
+            source -> 0.0 to initial
+            else -> {
+                val resultField = dist.alignedMap(field) { distField, (currentDist, value) ->
+                    distField + currentDist to accumulate(value)
+                }
+                resultField.fold(POSITIVE_INFINITY to POSITIVE_INFINITY) { acc, value ->
+                    if (value.first < acc.first) value else acc
+                }
             }
         }
     }.second
